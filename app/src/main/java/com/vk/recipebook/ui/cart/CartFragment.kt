a2224b84b927ac01454232ca0470d955560a88cb
vk <valeriia.koltsova@gmail.com>
2021-03-08 21:41:14 -0500

    created test api request for searchRecipes

--- app/src/main/java/com/vk/recipebook/ui/cart/CartFragment.kt
+++ app/src/main/java/com/vk/recipebook/ui/cart/CartFragment.kt
@@ -4,28 +4,66 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.vk.recipebook.R
+import com.vk.recipebook.data.SearchParameters
+import com.vk.recipebook.dataSources.RemoteDataSource
+import com.vk.recipebook.databinding.FragmentCartBinding
+import kotlinx.coroutines.launch
 
-class CartFragment : Fragment() {
+class CartFragment : Fragment(R.layout.fragment_cart) {
 
-    private lateinit var homeViewModel: CartViewModel
+
+    val viewModel: CartViewModel by viewModels()
+
+    private lateinit var binding: FragmentCartBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(CartViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_cart, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+
+        val root = inflater.inflate(
+            R.layout.fragment_cart, container, false
+        )
+        /*val textView: TextView = root.findViewById(R.id.text_notifications)
+        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
-        })
+        })*/
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCartBinding.bind(view)
+        val search = binding.testSearch
+        val searchButton = binding.testSearchButton
+        val searchResults = binding.testViewResults
+
+        searchButton.setOnClickListener {
+            lifecycleScope.launch{
+                val parameters = SearchParameters(search.text.toString())
+                try {
+                    val response = RemoteDataSource.searchRecipes(parameters)
+                    if (response.isEmpty())
+                        searchResults.text = "Sorry, no results"
+                    else
+                        searchResults.text = response.toString()
+                } catch (e: Exception){
+                    searchResults.text = "Error"
+                }
+
+
+
+
+            }
+        }
+    }
+
 }
